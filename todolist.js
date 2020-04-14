e7902f1f62309a43ec06f40070322b9e895bb3a1
Luiz Silva <luizsilva.ee@gmail.com>
2020-04-14 15:08:04 -0300

    Adicionada funcionalidade de excluir elementos da lista de afazeres

--- todolist.js
+++ todolist.js
@@ -8,15 +8,29 @@ let todos = [
   'Praticar esportes'
 ]
 
+
 function renderTodos(){
   listElement.innerHTML = ''
+
   for (todo of todos){
     let todoElement = document.createElement('li')
     let todoText = document.createTextNode(todo)
+
+    let linkElement = document.createElement('a')
+    linkElement.setAttribute('href', '#')
+    let linkText = document.createTextNode('Excluir')
+
+    let position = todos.indexOf(todo)
+    linkElement.setAttribute('onclick', 'deleteTodo('+position+')')
+
+    linkElement.appendChild(linkText)
     todoElement.appendChild(todoText)
+    todoElement.appendChild(linkElement)
     listElement.appendChild(todoElement)
   }
 }
+
+renderTodos()
 
 function newTodo(){
   let todoText = inputElement.value
@@ -26,4 +40,8 @@ function newTodo(){
 }
 
 buttonElement.onclick = newTodo
-renderTodos()
+
+function deleteTodo(position){
+  todos.splice(position, 1)
+  renderTodos()
+}
